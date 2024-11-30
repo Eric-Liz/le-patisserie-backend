1858053d6e426108f36b01e7a09a29333cb9edbc
Eric-Liz <eric.liz@salsatechnology.com>
2024-11-30 14:47:15 -0300

    add eureka structure with api-gateway

--- src/main/java/com/example/lepatisseriebackend/product/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.lepatisseriebackend.product;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
